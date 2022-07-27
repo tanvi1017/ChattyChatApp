398e187816ef3cba43b392c47b5dcb1181d0edb7
tanvi1017 <tripathi.tanvi1996@gmail.com>
2022-07-27 14:40:46 +0530

    Initial commit

---
+++ app/src/main/java/com/tanvi/mychatapp/LogoutActivity.kt
@@ -0,0 +1,11 @@
+package com.tanvi.mychatapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LogoutActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_logout)
+    }
+}
